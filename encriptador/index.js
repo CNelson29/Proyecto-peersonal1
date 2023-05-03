47d24c61ba8cd29fcc389629c3d29058e1f054af
CNelson29 <c.tiburciomolano@gmail.com>
2023-05-02 22:31:30 -0400

    function encriptar

--- encriptador/index.js
+++ encriptador/index.js
@@ -1,8 +1,8 @@
 function encriptar(){
     let text = document.getElementById("text").value;
-   // let tituloMensaje = document.getElementById("titulo-mensaje");
-   // let parrafo = document.getElementById("parrafo");//
-   // let imagenes = document.getElementById("imagenes");//
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let imagenes = document.getElementById("imagenes");
 
     let textCifrado = text 
     .replace(/e/gi, "10")
@@ -11,14 +11,16 @@ function encriptar(){
     .replace(/o/gi, "11")
     .replace(/u/gi, "12");  
 
-    if (document.getElementById("text").value.length  != 0){
+    if (text.length  != 0){
 
         document.getElementById("text").value = textCifrado;
-        document.getElementById("titulo-mensaje").textContent = "Texto encriptado safistactoriamente!!"
-        document.getElementById("parrafo").textContent = "";
-        document.getElementById("imagenes").src = "./imagenes/encriptado.jpg";
+        tituloMensaje.textContent = "Texto encriptado safistactoriamente!!";
+        parrafo.textContent = "";
+        imagenes.src = "./imagenes/encriptado.jpg";
     } else{
-        document.getElementById("imagenes").src = "./imagenes/imagenes.png";
+        imagenes.src = "./imagenes/imagenes.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         alert("Desbes incresar algun texto para encriptar");
     }
        
