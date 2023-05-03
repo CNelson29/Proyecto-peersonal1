8cb8d70194e69618b5249514e24704d05161ea2f
CNelson29 <c.tiburciomolano@gmail.com>
2023-05-02 22:37:59 -0400

    agregado desencriptar

--- encriptador/index.js
+++ encriptador/index.js
@@ -21,8 +21,39 @@ function encriptar(){
         imagenes.src = "./imagenes/imagenes.png";
         tituloMensaje.textContent = "Ningun mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        alert("Desbes incresar algun texto para encriptar");
+        alert("Desbes ingresar algun texto para encriptar");
     }
        
+
+}
+
+function desencriptar(){
+
+    let text = document.getElementById("text").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let imagenes = document.getElementById("imagenes");
+
+    
+    let textCifrado = text 
+    .replace(/10/gi, "e")
+    .replace(/22/gi, "i")
+    .replace(/21/gi, "a")
+    .replace(/11/gi, "o")
+    .replace(/12/gi, "u"); 
+    
+    if (text.length  != 0){
+
+        document.getElementById("text").value = textCifrado;
+        tituloMensaje.textContent = "Texto desencriptado safistactoriamente!!";
+        parrafo.textContent = "";
+        imagenes.src = "./imagenes/desencriptado.jpg";
+    } else{
+        imagenes.src = "./imagenes/imagenes.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Desbes ingresar algun texto para encriptar");
+    }
+
 
 }
